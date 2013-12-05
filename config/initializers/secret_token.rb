6fa3d353f344736ad6922cb828b0d4fae547a967
wanway <wanway0311@gmail.com>
2013-12-05 12:47:44 +0800

    add a new blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '7a7dc15c74d61db5d96d919887e872b5b993facb5c11b72ff0c16e4009d730b4f3a0570c7dae05271a0defa18eafd9033b31381a89a0cc9f1ec8dd91a59fe4a5'
